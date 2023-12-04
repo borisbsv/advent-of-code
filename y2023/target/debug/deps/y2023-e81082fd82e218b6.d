74d181cecc04475e61955d6b90a529071bce3703
Boris Stoyanov <sboris@vmware.com>
2023-12-04 22:44:30 +0200

    more target removal
    Signed-off-by: Boris Stoyanov <sboris@vmware.com>

--- y2023/target/debug/deps/y2023-e81082fd82e218b6.d
+++
@@ -1,9 +0,0 @@
-/Users/sboris/workspace/go/src/github.com/borisbsv/advent-of-code/y2023/target/debug/deps/y2023-e81082fd82e218b6: src/main.rs src/one/mod.rs src/one/solve.rs src/util/mod.rs src/util/read.rs
-
-/Users/sboris/workspace/go/src/github.com/borisbsv/advent-of-code/y2023/target/debug/deps/y2023-e81082fd82e218b6.d: src/main.rs src/one/mod.rs src/one/solve.rs src/util/mod.rs src/util/read.rs
-
-src/main.rs:
-src/one/mod.rs:
-src/one/solve.rs:
-src/util/mod.rs:
-src/util/read.rs:
